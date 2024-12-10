86ceaa22f77d9a10f894260317b9178fd56bc58e
Artem Chetverikov <av.chetverikov@tensor.ru>
2024-12-10 20:27:44 +0500

    *

--- myproject/task1/models.py
+++ myproject/task1/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Buyer(models.Model):
     name = models.CharField(max_length=30)
     balance = models.DecimalField(decimal_places=2, max_digits=10)
-    age = models.IntegerField()
+    age = models.PositiveIntegerField()
 
 
 class Game(models.Model):
